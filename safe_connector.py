aaaeda43f107d91c4444091ea10901e76f7a7b55
marcin <szateks@gmail.com>
2018-09-22 09:08:20 +0200

    Added safe_connector class to be able sending signals from depths of python thread to qt objects. Only one way communication.

---
+++ safe_connector.py
@@ -0,0 +1,65 @@
+from PyQt5 import QtCore
+import threading
+import socket
+from queue import Queue
+
+
+class SafeConnector(QtCore.QObject):
+
+    signal = QtCore.pyqtSignal(object)
+
+    def __init__(self, parent=None):
+        # initialise in Qt thread
+        super(QtCore.QObject, self).__init__(parent)
+        self._queue = Queue()
+        self.rsock, self.wsock = socket.socketpair()
+        self.notifier = QtCore.QSocketNotifier(
+            self.rsock.fileno(), QtCore.QSocketNotifier.Read)
+        self.notifier.activated.connect(self.recv)
+
+    def recv(self):
+        # runs in Qt thread after it's been woken up by QSocketNotifier
+        self.rsock.recv(1)
+        self.signal.emit(self._queue.get())
+
+    def input(self, msg):
+        # runs in any thread
+        self._queue.put(msg)
+        self.wsock.send(b'1')
+
+
+
+if __name__ == '__main__':
+
+    from PyQt5 import QtCore, QtWidgets
+    import time
+
+
+    class PythonThread(threading.Thread):
+        def __init__(self, connector, *args, **kwargs):
+            threading.Thread.__init__(self, *args, **kwargs)
+            self.connector = connector
+            self.daemon = True
+
+        def emit_signal(self):
+            # self.connector.emit(QtCore.pyqtSignal(str,name='sig'), str(time.time()))
+            self.connector.input(str(time.strftime('%H:%M:%S%')) + str(time.process_time()))
+
+        def run(self):
+            while True:
+                time.sleep(1)
+                self.emit_signal()
+
+
+    app = QtWidgets.QApplication([])
+    mainwin = QtWidgets.QMainWindow()
+    label = QtWidgets.QLabel(mainwin)
+    mainwin.setCentralWidget(label)
+
+    connector = SafeConnector()
+    python_thread = PythonThread(connector)
+    connector.signal.connect(label.setText)
+    python_thread.start()
+
+    mainwin.show()
+    app.exec()
